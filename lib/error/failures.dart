237f2a06fc7cc894411209cf86df71f757b7c8f3
abdullahAlwajih <pro.abdullah.alwajih@gmail.com>
2022-09-17 11:08:42 +0300

    update SuperTranslations

--- lib/error/failures.dart
+++ lib/error/failures.dart
@@ -4,89 +4,47 @@ abstract class Failure {
   static String exceptionToMessage(Exceptions exception) {
     switch (exception.runtimeType) {
       case InternalServerErrorException:
-        return exception.message ??
-            SuperTranslations.of(
-                    SuperFlutter.navigatorKey.currentState!.context)!
-                .errorServerException;
+        return exception.message ?? SuperTranslations.of.errorServerException;
       case ConflictException:
-        return exception.message ??
-            SuperTranslations.of(
-                SuperFlutter.navigatorKey.currentState!.context)!
-                .errorConflictException;
+        return exception.message ?? SuperTranslations.of.errorConflictException;
       case CacheException:
-        return exception.message ??
-            SuperTranslations.of(
-                SuperFlutter.navigatorKey.currentState!.context)!
-                .errorCacheException;
+        return exception.message ?? SuperTranslations.of.errorCacheException;
       case NoContentException:
-        return exception.message ??
-            SuperTranslations.of(
-                SuperFlutter.navigatorKey.currentState!.context)!
-                .errorEmptyException;
+        return exception.message ?? SuperTranslations.of.errorEmptyException;
       case ConnectionException:
         return exception.message ??
-            SuperTranslations.of(
-                SuperFlutter.navigatorKey.currentState!.context)!
-                .errorConnectionException;
+            SuperTranslations.of.errorConnectionException;
       case NotFoundException:
-        return exception.message ??
-            SuperTranslations.of(
-                SuperFlutter.navigatorKey.currentState!.context)!
-                .errorNotFoundException;
+        return exception.message ?? SuperTranslations.of.errorNotFoundException;
       case NotAcceptableException:
-        return exception.message ??
-            SuperTranslations.of(
-                SuperFlutter.navigatorKey.currentState!.context)!
-                .errorInvalidException;
+        return exception.message ?? SuperTranslations.of.errorInvalidException;
       case GoneException:
-        return exception.message ??
-            SuperTranslations.of(
-                SuperFlutter.navigatorKey.currentState!.context)!
-                .errorExpireException;
+        return exception.message ?? SuperTranslations.of.errorExpireException;
 
       case UserExistsException:
         return exception.message ??
-            SuperTranslations.of(
-                SuperFlutter.navigatorKey.currentState!.context)!
-                .errorUserExistsException;
+            SuperTranslations.of.errorUserExistsException;
       case PasswordException:
-        return exception.message ??
-            SuperTranslations.of(
-                SuperFlutter.navigatorKey.currentState!.context)!
-                .errorPasswordException;
+        return exception.message ?? SuperTranslations.of.errorPasswordException;
 
       case UnauthorizedException:
         return exception.message ??
-            SuperTranslations.of(
-                SuperFlutter.navigatorKey.currentState!.context)!
-                .errorUnauthenticatedException;
+            SuperTranslations.of.errorUnauthenticatedException;
 
       case ForbiddenException:
         return exception.message ??
-            SuperTranslations.of(
-                SuperFlutter.navigatorKey.currentState!.context)!
-                .errorUnexpectedException;
+            SuperTranslations.of.errorUnexpectedException;
       case BlockedException:
-        return exception.message ??
-            SuperTranslations.of(
-                SuperFlutter.navigatorKey.currentState!.context)!
-                .errorBlockedException;
+        return exception.message ?? SuperTranslations.of.errorBlockedException;
       case EmptyException:
-        return exception.message ??
-            SuperTranslations.of(
-                SuperFlutter.navigatorKey.currentState!.context)!
-                .errorEmptyException;
+        return exception.message ?? SuperTranslations.of.errorEmptyException;
 
       case CustomException:
         return exception.message ??
-            SuperTranslations.of(
-                SuperFlutter.navigatorKey.currentState!.context)!
-                .errorUnexpectedException;
+            SuperTranslations.of.errorUnexpectedException;
       default:
         return exception.message ??
-            SuperTranslations.of(
-                SuperFlutter.navigatorKey.currentState!.context)!
-                .errorUnexpectedException;
+            SuperTranslations.of.errorUnexpectedException;
     }
   }
 }
--- lib/super_flutter.dart
+++ lib/super_flutter.dart
@@ -1,13 +1,7 @@
 library super_flutter;
 
-import 'package:flutter/material.dart';
 import 'package:super_flutter/super_flutter.dart';
 export 'package:super_localizations/super_localizations.dart';
 export 'package:super_repository/super_repository.dart';
 
 part 'error/failures.dart';
-
-
-abstract class SuperFlutter {
-  static GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
-}
