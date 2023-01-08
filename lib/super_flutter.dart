2bb4d0cac4960412cb4ca315ec38fb0807a1e1d8
Omar Taha <omartaha.tech7@gmail.com>
2023-01-08 11:24:17 +0300

    Update super_flutter.dart

--- lib/super_flutter.dart
+++ lib/super_flutter.dart
@@ -1,7 +1,6 @@
 library super_flutter;
 
 import 'package:super_flutter/super_flutter.dart';
-export 'package:super_localizations/super_localizations.dart';
 export 'package:super_repository/super_repository.dart';
 
 part 'error/errors.dart';
