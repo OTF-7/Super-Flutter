c0a89f11d0d2feda43538a224dae659a016809ea
Omar Taha <omartaha.tech7@gmail.com>
2023-01-09 13:49:48 +0300

    Register superFlutterLocalization

--- lib/super_flutter.dart
+++ lib/super_flutter.dart
@@ -2,9 +2,22 @@ library super_flutter;
 
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/super_flutter_localizations.dart';
+import 'package:intl/intl.dart' as intl;
 import 'package:super_flutter/super_flutter.dart';
 
 export 'package:super_repository/super_repository.dart';
 
 part 'error/errors.dart';
 part 'error/firebase_errors.dart';
+
+abstract class SuperFlutterTranslations {
+  static GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+  SuperFlutterTranslations(String locale)
+      : localeName = intl.Intl.canonicalizedLocale(locale.toString());
+
+  final String localeName;
+
+  static AppLocalizations get of => Localizations.of<AppLocalizations>(
+      navigatorKey.currentState!.context, AppLocalizations)!;
+}
